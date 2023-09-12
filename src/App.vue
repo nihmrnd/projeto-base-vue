587f5850413fba2abe3ce5235cf244b406828a4d
Nicole Arruda <nicole.fernandamiranda@gmail.com>
2023-09-12 11:02:05 -0400

    setup vue inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>    
+    <div class="wrapper">
+      <HelloWorld msg="Hello world!" />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
